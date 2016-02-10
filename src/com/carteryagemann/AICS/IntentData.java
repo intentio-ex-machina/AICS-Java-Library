af64435cb3c4cb61047bdb8447b8296c96273405
Carter Yagemann <carter.yagemann@gmail.com>
2016-02-09 19:41:20 -0500

    Implemented IntentData and some basic tests for it.

--- src/com/carteryagemann/AICS/IntentData.java
+++ src/com/carteryagemann/AICS/IntentData.java
@@ -16,6 +16,7 @@
 package com.carteryagemann.AICS;
 
 import java.nio.ByteBuffer;
+import java.nio.BufferUnderflowException;
 
 /**
  * IntentData is an object for storing the contents of the logged Android
@@ -26,6 +27,163 @@ import java.nio.ByteBuffer;
  */
 public class IntentData {
     
+    private int FLAGS;
+    private int ACTION_SIZE;
+    private int DATA_SIZE;
+    private int CATEGORY_SIZE;
+    private int TYPE_SIZE;
+    private int CLIPDATA_SIZE;
+    private int EXTRAS_SIZE;
+    private String ACTION;
+    private String DATA;
+    private String CATEGORY;
+    private String TYPE;
+    private byte[] CLIPDATA;
+    private byte[] EXTRAS;
+    
+    /**
+     * The default constructor initializes all the fields as null and size 0.
+     */
+    IntentData() {
+        FLAGS = 0;
+        ACTION_SIZE = 0;
+        DATA_SIZE = 0;
+        CATEGORY_SIZE = 0;
+        TYPE_SIZE = 0;
+        CLIPDATA_SIZE = 0;
+        EXTRAS_SIZE = 0;
+    }
+    
+    /**
+     * Creates a new IntentData object from a ByteBuffer.
+     * 
+     * @param buffer The ByteBuffer containing a flattened IntentData object.
+     * @throws BufferUnderflowException If the buffer is malformed.
+     */
+    IntentData(ByteBuffer buffer) throws BufferUnderflowException {
+        try {
+            FLAGS = buffer.getInt();
+            ACTION_SIZE = buffer.getInt();
+            DATA_SIZE = buffer.getInt();
+            CATEGORY_SIZE = buffer.getInt();
+            TYPE_SIZE = buffer.getInt();
+            CLIPDATA_SIZE = buffer.getInt();
+            EXTRAS_SIZE = buffer.getInt();
+            
+            if (ACTION_SIZE > 0) {
+                byte[] temp = new byte[ACTION_SIZE];
+                buffer.get(temp);
+                ACTION = new String(temp);
+            }
+            
+            if (DATA_SIZE > 0) {
+                byte[] temp = new byte[DATA_SIZE];
+                buffer.get(temp);
+                DATA = new String(temp);
+            }
+            
+            if (CATEGORY_SIZE > 0) {
+                byte[] temp = new byte[CATEGORY_SIZE];
+                buffer.get(temp);
+                CATEGORY = new String(temp);
+            }
+            
+            if (TYPE_SIZE > 0) {
+                byte[] temp = new byte[TYPE_SIZE];
+                buffer.get(temp);
+                TYPE = new String(temp);
+            }
+            
+            if (CLIPDATA_SIZE > 0) {
+                CLIPDATA = new byte[CLIPDATA_SIZE];
+                buffer.get(CLIPDATA);
+            }
+            
+            if (EXTRAS_SIZE > 0) {
+                EXTRAS = new byte[EXTRAS_SIZE];
+                buffer.get(EXTRAS);
+            }
+        } catch (BufferUnderflowException e) {
+            throw e;
+        }
+    }
+    
+    /**
+     * Creates a new IntentData object from an array of bytes.
+     * 
+     * @param array The array to parse into an IntentData object.
+     * @throws BufferUnderflowException If the array is malformed.
+     */
+    IntentData(byte[] array) throws BufferUnderflowException {
+        this(ByteBuffer.wrap(array));
+    }
+    
+    public IntentData setFlags(int flags) {
+        FLAGS = flags;
+        return this;
+    }
+    
+    public IntentData setAction(String action) {
+        ACTION = action;
+        ACTION_SIZE = action.length();
+        return this;
+    }
+    
+    public IntentData setData(String data) {
+        DATA = data;
+        DATA_SIZE = data.length();
+        return this;
+    }
+    
+    public IntentData setCategory(String category) {
+        CATEGORY = category;
+        CATEGORY_SIZE = category.length();
+        return this;
+    }
+    
+    public IntentData setType(String type) {
+        TYPE = type;
+        TYPE_SIZE = type.length();
+        return this;
+    }
+    
+    public IntentData setClipData(byte[] data) {
+        CLIPDATA = data;
+        CLIPDATA_SIZE = data.length;
+        return this;
+    }
+    
+    public IntentData setExtras(byte[] extras) {
+        EXTRAS = extras;
+        EXTRAS_SIZE = extras.length;
+        return this;
+    }
+    
+    public int getFlags() { return FLAGS; }
+    
+    public String getAction() { return ACTION; }
+    
+    public String getData() { return DATA; }
+    
+    public String getCategory() { return CATEGORY; }
+    
+    public String getType() { return TYPE; }
+    
+    public byte[] getClipData() { return CLIPDATA; }
+    
+    public byte[] getExtras() { return EXTRAS; }
+    
+    /**
+     * Calculates the total number of bytes needed to write this IntentData.
+     * 
+     * @return The number of bytes this IntentData will use when flattened into
+     * a sequence of bytes.
+     */
+    public int getSize() {
+        return 32 * 7 + ACTION_SIZE + DATA_SIZE + CATEGORY_SIZE + TYPE_SIZE
+                + CLIPDATA_SIZE + EXTRAS_SIZE;
+    }
+    
     /**
      * Puts the contents of IntentData into a ByteBuffer which is ready to be
      * written to an AICS file.
@@ -33,8 +191,24 @@ public class IntentData {
      * @return A ByteBuffer containing the contents of the IntentData.
      */
     protected ByteBuffer toByteBuffer() {
-        //TODO Implement method
-        System.err.println("Method IntentData.toByteBuffer not implemented!");
-        return ByteBuffer.allocate(100);
+        // Created fix-sized part of buffer
+        ByteBuffer buffer = ByteBuffer.allocate(getSize())
+                .putInt(FLAGS)
+                .putInt(ACTION_SIZE)
+                .putInt(DATA_SIZE)
+                .putInt(CATEGORY_SIZE)
+                .putInt(TYPE_SIZE)
+                .putInt(CLIPDATA_SIZE)
+                .putInt(EXTRAS_SIZE);
+        
+        // Create variable-sized part of buffer
+        if (ACTION_SIZE > 0) buffer.put(ACTION.getBytes());
+        if (DATA_SIZE > 0) buffer.put(DATA.getBytes());
+        if (CATEGORY_SIZE > 0) buffer.put(CATEGORY.getBytes());
+        if (TYPE_SIZE > 0) buffer.put(TYPE.getBytes());
+        if (CLIPDATA_SIZE > 0) buffer.put(CLIPDATA);
+        if (EXTRAS_SIZE > 0) buffer.put(EXTRAS);
+        
+        return buffer;
     }
 }
--- test/com/carteryagemann/AICS/IntentDataTest.java
+++ test/com/carteryagemann/AICS/IntentDataTest.java
@@ -15,9 +15,12 @@
  */
 package com.carteryagemann.AICS;
 
+import java.util.Random;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -28,6 +31,8 @@ import static org.junit.Assert.*;
  * @author Carter Yagemann <carter.yagemann@gmail.com>
  */
 public class IntentDataTest {
+    
+    private Random RANDOM = new Random(System.currentTimeMillis());
     
     public IntentDataTest() {
     }
@@ -55,11 +60,118 @@ public class IntentDataTest {
     public void testToByteBuffer() {
         System.out.println("toByteBuffer");
         IntentData instance = new IntentData();
-        ByteBuffer expResult = null;
-        ByteBuffer result = instance.toByteBuffer();
+        byte[] expResult = new byte[224];
+        Arrays.fill(expResult, (byte) 0);
+        byte[] result = instance.toByteBuffer().array();
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of setFlags method, of class IntentData.
+     */
+    @Test
+    public void testSetFlags() {
+        System.out.println("setFlags");
+        int flags = RANDOM.nextInt();
+        IntentData instance = new IntentData().setFlags(flags);
+        assertEquals(flags, instance.getFlags());
+    }
+
+    /**
+     * Test of setAction method, of class IntentData.
+     */
+    @Test
+    public void testSetAction() {
+        System.out.println("setAction");
+        String action = "android.intent.action.AIRPLANE_MODE";
+        IntentData instance = new IntentData().setAction(action);
+        assertEquals(action, instance.getAction());
+    }
+
+    /**
+     * Test of setData method, of class IntentData.
+     */
+    @Test
+    public void testSetData() {
+        System.out.println("setData");
+        String data = "file:///tmp/android.txt";
+        IntentData instance = new IntentData().setData(data);
+        assertEquals(data, instance.getData());
+    }
+
+    /**
+     * Test of setCategory method, of class IntentData.
+     */
+    @Test
+    public void testSetCategory() {
+        System.out.println("setCategory");
+        String category = "android.intent.category.APP_BROWSER";
+        IntentData instance = new IntentData().setCategory(category);
+        assertEquals(category, instance.getCategory());
+    }
+
+    /**
+     * Test of setType method, of class IntentData.
+     */
+    @Test
+    public void testSetType() {
+        System.out.println("setType");
+        String type = "text/plain";
+        IntentData instance = new IntentData().setType(type);
+        assertEquals(type, instance.getType());
+    }
+
+    /**
+     * Test of setClipData method, of class IntentData.
+     */
+    @Test
+    public void testSetClipData() {
+        System.out.println("setClipData");
+        byte[] data = new byte[100];
+        RANDOM.nextBytes(data);
+        IntentData instance = new IntentData().setClipData(data);
+        assertEquals(data, instance.getClipData());
+    }
+
+    /**
+     * Test of setExtras method, of class IntentData.
+     */
+    @Test
+    public void testSetExtras() {
+        System.out.println("setExtras");
+        byte[] extras = new byte[100];
+        RANDOM.nextBytes(extras);
+        IntentData instance = new IntentData().setExtras(extras);
+        assertEquals(extras, instance.getExtras());
+    }
+
+    /**
+     * Test of getSize method, of class IntentData.
+     */
+    @Test
+    public void testGetSize() {
+        System.out.println("getSize");
+        String action = "android.intent.action.AIRPLANE_MODE";
+        String data = "file:///tmp/android.txt";
+        String category = "android.intent.category.APP_BROWSER";
+        String type = "text/plain";
+        byte[] clipdata = new byte[100];
+        byte[] extras = new byte[100];
+        RANDOM.nextBytes(clipdata);
+        RANDOM.nextBytes(extras);
+        IntentData instance = new IntentData()
+                .setFlags(0)
+                .setAction(action)
+                .setData(data)
+                .setCategory(category)
+                .setType(type)
+                .setClipData(clipdata)
+                .setExtras(extras);
+        int expResult = 32 * 7 + action.length() + data.length()
+                + category.length() + type.length() + clipdata.length
+                + extras.length;
+        int result = instance.getSize();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
